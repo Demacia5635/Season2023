fc83884a4c08df9ef121ea7886878dadb9201445
Greninja <BobbyBoten@gmail.com>
2023-02-20 00:03:15 +0200

    Add error fixes

--- src/main/java/frc/robot/subsystems/parallelogram/ParallelConstants.java
+++ src/main/java/frc/robot/subsystems/parallelogram/ParallelConstants.java
@@ -7,12 +7,14 @@ public final class ParallelConstants {
     public static final int PORT_NUMBER_PARALLEL_MOTOR = 30;
     public static final boolean MOTOR_INVERT_TYPE = true;
     public static final double PARALLEL_LENGTH = 0.7; // In meters
-    public static final double ROBOT_HEIGHT = 0.58;
+    public static final double ROBOT_HEIGHT = 0.61; // 0.58
 
     public static final int GYRO_PORT_NUMBER = -4;
     public static final double GYRO_OFFSET = 0;
 
-    public static final double PULSE_PER_ANGLE = 1024;
+    public static final double HEIGHT_ANGLE_ERROR = 14.47;
+    public static final double TRANSMITION_RATIO_ERROR = 1.1275;
+    public static final double PULSE_PER_ANGLE = 1024 / TRANSMITION_RATIO_ERROR;
 
     public static final double PULI_RADIUS = 1;
     public static final double PULI_PERIMETER = 2 * PULI_RADIUS * Math.PI;
@@ -20,7 +22,7 @@ public final class ParallelConstants {
     public static final double KS_VELOCITY = 0;
     public static final double KV_VELOCITY = 0;
 
-    public static final double DIGITAL_INPUT_ANGLE = 125.76;
+    public static final double DIGITAL_INPUT_ANGLE = 129.1;
     public static final int PORT_DIGITAL_INPUT = 0;
 
     public static final double KP_POSITION = 0.043;
