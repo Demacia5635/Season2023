272502988635af9e02164cd29816c94ef6b2f7d4
yoavnahumus <yoavnahum2005@gmail.com>
2023-02-22 00:11:15 +0200

    added leds temporarily

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,11 +5,15 @@
 package frc.robot;
 
 import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.chassis.Drive;
@@ -41,10 +45,8 @@ public class RobotContainer {
     private static RobotContainer instance;
     public Parallelogram parallelogram;
     private final Gripper gripper;
-
-    public final class RobotFinalCommands {
-
-    }
+    private final AddressableLED leds;
+    private final AddressableLEDBuffer buffer;
 
     /**
      * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -57,6 +59,11 @@ public class RobotContainer {
         gripper = new Gripper(GripperConstants.MOTOR_ID);
         SmartDashboard.putData(gripper);
         configureButtonBindings();
+
+        leds = new AddressableLED(0);
+        leds.setLength(64);
+        buffer = new AddressableLEDBuffer(64);
+        leds.start();
 
         SmartDashboard.putData("pickup", new PickUp(parallelogram, chassis));
     }
@@ -104,6 +111,26 @@ public class RobotContainer {
         main.b().onTrue(unload);
         main.y().onTrue(new GotoCommunity(chassis, main.getHID()));
         main.x().onTrue(new GoUpRamp(chassis, 1.5));
+
+        secondary.rightBumper().onTrue(new InstantCommand(() -> {
+            if (!buffer.getLED(0).equals(new Color(168, 0, 230))) {
+                for (int i = 0; i < 64; i++) {
+                    buffer.setRGB(i, 168, 0, 230);
+                }
+            } else {
+                for (int i = 0; i < 64; i++) {
+                    buffer.setRGB(i, 255, 140, 0);
+                } 
+            }
+            leds.setData(buffer);
+        }).ignoringDisable(true));
+
+        secondary.leftBumper().onTrue(new InstantCommand(() -> {
+            for (int i = 0; i < 64; i++) {
+                buffer.setRGB(i, 0, 0, 0);
+            }
+            leds.setData(buffer);
+        }).ignoringDisable(true));
 
         // controller.leftBumper().onTrue(loadIfInPlace);
         // controller.rightBumper().onTrue(unloadIfInPlace);
