0af822c5eec614c9cd9455429651e63a44c15486
YoavNahumus <yoavnahum2005@gmail.com>
2023-02-24 18:26:10 +0200

    formatted stuff

--- src/main/java/frc/robot/GenerateAutonomous.java
+++ src/main/java/frc/robot/GenerateAutonomous.java
@@ -5,7 +5,6 @@
 package frc.robot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.chassis.GotoNodes;
@@ -16,14 +15,13 @@ import frc.robot.subsystems.gripper.Gripper;
 
 /** Add your docs here. */
 public class GenerateAutonomous {
-  private SendableChooser placeGamePeace;
-  private SendableChooser exitCommunity;
-  private SendableChooser climb;
+  private SendableChooser<Boolean> placeGamePeace;
+  private SendableChooser<Boolean> exitCommunity;
+  private SendableChooser<Boolean> climb;
   private GotoNodes gotoNodes;
   private LeaveCommunity leaveCommunity;
   private Gripper gripper;
   private Chassis chassis;
-  private boolean finish;
 
   /** Creates a new GenerateAutonomous. */
   public GenerateAutonomous(GotoNodes gotoNodes, LeaveCommunity leaveCommunity, Gripper gripper, Chassis chassis) {
@@ -31,30 +29,28 @@ public class GenerateAutonomous {
     this.leaveCommunity = leaveCommunity;
     this.gripper = gripper;
     this.chassis = chassis;
-    finish = false;
     
-    placeGamePeace = new SendableChooser<Boolean>();
-        placeGamePeace.setDefaultOption("Top", true);
-        placeGamePeace.addOption("Bottom", false);
-        SmartDashboard.putData(placeGamePeace);
+    placeGamePeace = new SendableChooser<>();
+    placeGamePeace.setDefaultOption("Top", true);
+    placeGamePeace.addOption("Bottom", false);
+    SmartDashboard.putData(placeGamePeace);
 
-        exitCommunity = new SendableChooser<Boolean>();
-        exitCommunity.setDefaultOption("Leave", true);
-        exitCommunity.addOption("Dont Leave", false);
-        SmartDashboard.putData(leaveCommunity);
+    exitCommunity = new SendableChooser<>();
+    exitCommunity.setDefaultOption("Leave", true);
+    exitCommunity.addOption("Dont Leave", false);
+    SmartDashboard.putData(leaveCommunity);
 
-        climb = new SendableChooser<Boolean>();
-        climb.setDefaultOption("Top", true);
-        climb.addOption("Bottom", false);
-        SmartDashboard.putData(climb);
+    climb = new SendableChooser<>();
+    climb.setDefaultOption("Top", true);
+    climb.addOption("Bottom", false);
+    SmartDashboard.putData(climb);
   }
 
   public SequentialCommandGroup getAutonomous(){
     SequentialCommandGroup autonomous = new SequentialCommandGroup(placeGamePeace.getSelected().equals(true) ? gotoNodes.andThen(gripper.getOpenCommand()) 
         : new InstantCommand(()-> System.out.println("didnt place Game Peace")).andThen(exitCommunity.getSelected().equals(true)?
-         new LeaveCommunity(chassis) : new InstantCommand(()-> System.out.println("didnt leave"))).andThen(climb.getSelected().equals(true)?
+         leaveCommunity : new InstantCommand(()-> System.out.println("didnt leave"))).andThen(climb.getSelected().equals(true)?
           new GotoRamp(chassis) :new InstantCommand(()->System.out.println("didnt climb"))));
-    finish = true;
     return autonomous;
   }
 }
