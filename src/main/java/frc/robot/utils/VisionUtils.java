02e17f663c6d261d0dd84b4290a68664c9beadbc
YoavNahumus <yoavnahum2005@gmail.com>
2023-03-13 22:17:37 +0200

    added precision to the limelight timestamp

--- src/main/java/frc/robot/utils/VisionUtils.java
+++ src/main/java/frc/robot/utils/VisionUtils.java
@@ -8,8 +8,8 @@ import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.networktables.NetworkTableEvent.Kind;
 import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEvent.Kind;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -62,6 +62,7 @@ public class VisionUtils {
      *         the measurement, or null if no target is found
      */
     public static Pair<Pose2d, Double> getVisionPose(NetworkTable limelightTable, LimeLight limelight) {
+        double timeStamp = Timer.getFPGATimestamp();
         double hasTarget = limelightTable.getEntry("tv").getDouble(0);
         if (hasTarget == 0)
             return null;
@@ -97,6 +98,6 @@ public class VisionUtils {
         }
         return new Pair<Pose2d, Double>(
                 new Pose2d(robotTranslation, robotRotation),
-                Timer.getFPGATimestamp() - (latency / 1000));
+                timeStamp - (latency / 1000));
     }
 }
