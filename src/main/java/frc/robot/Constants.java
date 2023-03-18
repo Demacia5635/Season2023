5d26e9f7ba76059c07dc7773acdef20401b9c06a
Greninja <BobbyBoten@gmail.com>
2023-03-18 20:40:22 +0200

    Parallelogram heights update, GoToNodesHalfManual update

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -38,7 +38,7 @@ public final class Constants {
     public static final double FIELD_WIDTH = 16.54; // in meters
     public static final double FIELD_HEIGHT = 8.02; // in meters
 
-    public static final double LOADING_ANGLE = 43;
+    public static final double LOADING_ANGLE = 44;
     public static final double DEPLOY_ANGLE = 20;
     public static final double DEPLOY_ANGLE_LOW = 60;
     // public static final double DEPLOY_ANGLE2 = 20;
